68eb448de9656d2e52ed12f361e6ba4e5bf3fa47
cys <cys@cys-Mac.local>
2019-08-16 12:10:58 +0800

    add function

--- plot_figure/utils.py
+++ plot_figure/utils.py
@@ -2,6 +2,7 @@ from utils import CHANNEL_LIST, remove_people_with_same_labels
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
 import numpy as np
+
 CHANNEL_LOCATION = np.array([[-27, 83, -3],
                              [27, 83, -3],
                              [-71, 51, -3],
@@ -73,4 +74,69 @@ def plot_rate_of_salient_features_in_each_channel(x, y):
     sc = plt.scatter(CHANNEL_LOCATION[:, 0], CHANNEL_LOCATION[:, 1],
                      c=ch_score, cmap=cm, s=700, vmin=np.min(ch_score), vmax=np.max(ch_score))
     plt.colorbar(sc)
+    plt.show()
+
+
+def plot_entropy_scale_value_on_diff_class(x, y, log, entropy_log, channel):
+    """plot scale v.s. value diagram on diff class (for entropy feature)
+
+    Args:
+        :param x: # people x # trials x # channels x # features
+        :param y: # people x # trials
+        :param log: # features
+        :param entropy_log: type of entropy_log = {'MSE', 'MPE', 'MDE', 'MFDE'}
+        :channel: select the channel which used to plot the diagram (channel is in the CHANNEL_LIST)
+    """
+
+    assert entropy_log in {'MSE', 'MPE', 'MDE', 'MFDE'}
+    assert channel in CHANNEL_LIST
+
+    title_dic = dict(MSE='Multisclae Entropy', MPE='Multisclae Permutation Entropy',
+                     MDE='Multisclae Dispersion Entropy', MFDE='Fluctuation-based Multisclae Dispersion Entropy')
+
+    x, y = remove_people_with_same_labels(x, y, label_type='rating')
+
+    index_of_entropy_feature = [i for i in range(len(log)) if log[i][:len(entropy_log)] == entropy_log]
+    assert len(index_of_entropy_feature) == 20
+
+    index_of_channel = np.where(CHANNEL_LIST == channel)[0][0]
+
+    x = x[:, :, index_of_channel, index_of_entropy_feature]  # people x # trials x # scales
+    x = x.reshape(-1, x.shape[2])  # (# people x # trials) x # scales
+    y = y.reshape(-1)  # (# people x # trials)
+    num_scale = x.shape[1]
+
+    pos_class_index = np.where(y > 4)[0]
+    neg_class_index = np.where(y < 4)[0]
+
+    x_pos = x[pos_class_index]
+    x_neg = x[neg_class_index]
+
+    scale_with_salient_p_value = [scale+1 for scale in range(
+        num_scale) if ttest_ind(x_neg[:, scale], x_pos[:, scale])[1] < 0.05]
+
+    pos_mean_value_list = np.array([np.mean(x_pos[:, scale]) for scale in range(num_scale)])
+    pos_std_value_list = np.array([np.std(x_pos[:, scale]) for scale in range(num_scale)])
+
+    neg_mean_value_list = np.array([np.mean(x_neg[:, scale]) for scale in range(num_scale)])
+    neg_std_value_list = np.array([np.std(x_neg[:, scale]) for scale in range(num_scale)])
+
+    plt.errorbar(np.arange(1, 1+num_scale), pos_mean_value_list, yerr=pos_std_value_list, ecolor='b',
+                 xuplims=True, xlolims=True, fmt='bo-', elinewidth=2, capsize=4, label='not MW')
+    plt.errorbar(np.arange(1, 1+num_scale), neg_mean_value_list, yerr=neg_std_value_list, ecolor='r',
+                 xuplims=True, xlolims=True, fmt='ro-', elinewidth=2, capsize=4, label='MW')
+
+    # the y axis of star
+    star_y_axis = max(np.max(pos_mean_value_list+pos_std_value_list),
+                      np.max(neg_mean_value_list+neg_std_value_list))+0.2
+    #star_y_axis = 4.7
+    # plot the star on scale with p value < 0.05
+    plt.scatter(scale_with_salient_p_value, [star_y_axis]*len(scale_with_salient_p_value), marker="*")
+
+    plt.xticks(np.linspace(1, num_scale, num_scale))
+    plt.xlabel('Scale')
+    plt.ylabel('{} value'.format(entropy_log))
+    plt.title(title_dic[entropy_log])
+    #plt.ylim(0.3, 4.8)
+    plt.legend(loc='lower right')
     plt.show()
